018c7d8439eaa0915366e8aac2ea6d4b669f3f22
Ider Kawabata <107132431+iderisadev@users.noreply.github.com>
2025-01-23 10:36:32 -0800

    Delete env.bat

--- env.bat
+++
@@ -1,5 +0,0 @@
-set SECRET_KEY=emiliano
-set GITHUB_CLIENT_ID=Ov23li3HaBQ3vP7L7kPq
-set GITHUB_CLIENT_SECRET=faa05d0c836a56553624fe18d5efd185793508e1
-set MONGO_DBNAME=WorldBuilding
-set MONGO_CONNECTION_STRING=mongodb+srv://iderkawabata:forumPass@worldbuild.07fti.mongodb.net/?retryWrites=true&w=majority&appName=WorldBuild
